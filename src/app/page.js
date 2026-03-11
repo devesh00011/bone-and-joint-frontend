8f18168a8cd9cd5b2e11d02cf79da7fe120c3160
devesh00011 <tarunmehra80790@gmail.com>
2026-03-11 09:11:25 +0530

    remove insurance

--- src/app/page.js
+++ src/app/page.js
@@ -18,7 +18,7 @@ export default function Home() {
       <OurServices />
       <BookAppointmentCTA />
       <WhyChooseUs />
-      <Insurance />
+      {/* <Insurance /> */}
       <Testimonials />
       <Faqs />
     </div>
