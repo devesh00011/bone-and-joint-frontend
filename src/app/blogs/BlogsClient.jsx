a7c77c9752a683a4446827a7454c90726f3a5f81
devesh00011 <tarunmehra80790@gmail.com>
2026-04-27 09:46:33 +0530

    updated code

--- src/app/blogs/BlogsClient.jsx
+++ src/app/blogs/BlogsClient.jsx
@@ -20,7 +20,7 @@ export default function BlogsClient() {
         'Travel',
         'Food'
     ]
-    const [selectedCategory, setSelectedCategory] = useState(null)
+    const [selectedCategory, setSelectedCategory] = useState('all')
     return (
         <div className='w-full h-full'>
 
@@ -29,19 +29,16 @@ export default function BlogsClient() {
                 <div className='max-w-330 mx-auto lg:px-6 px-3 lg:py-10 py-6'>
                     <div className='grid lg:grid-cols-[30%_auto] grid-cols-1 gap-10'>
                         <div>
-
-
                             <div className='p-5 rounded-2xl mb-6 bg-white h-fit shadow-xl border-2 border-gray-300'>
                                 <h2 className='relative text-2xl font-semibold text-gray-800 mb-5'>Categories
-                                    <div className='absolute top-full left-0 w-12 h-1 rounded-full bg-blue-700'></div>
+                                    <div className='absolute top-full left-0 w-12 h-1 rounded-full bg-[#00B4D8]'></div>
                                 </h2>
-                                <button onClick={() => setSelectedCategory('all')} className={`${selectedCategory == 'all' ? 'bg-blue-600 text-white' : 'bg-white text-bg-blue-600'} hover:bg-blue-600 hover:text-white  rounded-full cursor-pointer border-2 border-gray-400 hover:scale-[1.1] duration-100 px-4 py-1 hover:border-transparent border-bg-blue-600 m-2`}>All</button>
+                                <button onClick={() => setSelectedCategory('all')} className={`${selectedCategory == 'all' ? 'bg-[#00B4D8] text-white border-transparent' : 'bg-white text-bg-[#00B4D8] '} hover:bg-[#00B4D8] hover:text-white  rounded-full cursor-pointer border-2 border-gray-400 hover:scale-[1.1] duration-100 px-4 py-1 hover:border-transparent border-bg-[#00B4D8] m-2`}>All</button>
                                 {data.map((item, index) => {
                                     return (
-                                        <button onClick={() => setSelectedCategory(item)} key={index} className={`${selectedCategory == item ? 'bg-blue-600 text-white' : 'bg-white text-bg-blue-600'} rounded-full cursor-pointer hover:bg-blue-600 hover:text-white duration-100 border-2 border-gray-400 px-4 py-1 shadow-md hover:scale-[1.1] hover:border-transparent border-bg-blue-600 m-2.5`}>{item}</button>
+                                        <button onClick={() => setSelectedCategory(item)} key={index} className={`${selectedCategory == item ? 'bg-[#00B4D8] text-white border-transparent' : 'bg-white text-bg-[#00B4D8] '} rounded-full cursor-pointer hover:bg-[#00B4D8] hover:text-white duration-100 border-2 border-gray-400 px-4 py-1 shadow-md hover:scale-[1.1] hover:border-transparent border-bg-[#00B4D8] m-2.5`}>{item}</button>
                                     )
                                 })}
-
 
                             </div>
 
@@ -89,22 +86,20 @@ export default function BlogsClient() {
                         <div className='grid sm:grid-cols-2 gap-10'>
                             {[1, 2, 3, 4, 5, 6].map((item, index) => {
                                 return (
-                                    <Link key={index} href={`/blogs/category/${item}`}><div key={index} className='group border-2 cursor-pointer bg-white border-gray-200 hover:shadow-md rounded-2xl'>
-                                        <div className='w-full h-[190px] relative overflow-hidden'>
+                                    <Link key={index} href={`/blogs/category/${item}`}><div key={index} className='group border-2 cursor-pointer bg-white border-gray-200 hover:shadow-lg shadow-md rounded-2xl'>
+                                        <div className='w-full h-[190] relative overflow-hidden'>
                                             <Image alt='blog image' fill src={'/banner.jpg'} className='w-full h-full group-hover:scale-[1.1] duration-300  rounded-t-xl object-cover absolute left-0 top-0' />
                                         </div>
                                         <div className='p-5'>
-                                            <h2 className='text-xl font-semibold group-hover:text-blue-600 duration-300'>{index + 1}. What is difference between div and section ?</h2>
-                                            <p className='border-b-2 border-gray-200 pb-1.5 line-clamp-2 group-hover:text-blue-600 duration-300'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis dicta soluta placeat minus, delectus sint veritatis, ipsum id incidunt aperiam.</p>
+                                            <h2 className='text-xl font-semibold group-hover:text-[#00B4D8] duration-300'>{index + 1}. What is difference between div and section ?</h2>
+                                            <p className='border-b-2 border-gray-200 pb-1.5 line-clamp-2 group-hover:text-[#00B4D8] duration-300'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis dicta soluta placeat minus, delectus sint veritatis, ipsum id incidunt aperiam.</p>
                                             <p></p>
-                                            <p className='mt-1.5 text-gray-800 font-semibold'>Dr. Rajiv Chouhan</p>
+                                            <p className='mt-1.5 text-gray-800 font-semibold'>By Dr. Rajiv Chouhan</p>
                                             <p className='text-gray-800 font- '>21, March 2026 | <span> 9 min read time</span></p>
                                         </div>
                                     </div></Link>
                                 )
                             })}
-
-
                         </div>
                     </div>
 
--- src/app/blogs/category/[slug]/page.js
+++ src/app/blogs/category/[slug]/page.js
@@ -1,7 +1,62 @@
+import Image from 'next/image'
 import React from 'react'
 
 export default function Page() {
-  return (
-    <div>page</div>
-  )
+    return (
+        <div>
+            <section className='w-full h-full'>
+                <div className='max-w-330 mx-auto lg:px-6 px-3'>
+                    <div className='w-full h-[330] relative lg:mt-8 mt-4'>
+                        <Image alt='blog image' src={'/banner.jpg'} fill className='absolute object-cover rounded-xl top-0 left-0 w-full h-full' />
+                    </div>
+                    <div className='my-5 shadow-md p-5 rounded-xl'>
+
+                        <h1 className='lg:text-3xl text-2xl group my-2 font-semibold text-[#00B4D8] duration-300'>What is the benefit after get Bone and Joint Hospital Appointment ?
+                            <div className='w-[50] h-1.5 my-1 group-hover:w-[100] duration-300 rounded-full bg-[#00B4D8]'></div>
+                        </h1>
+
+                        <p className='mt-2 text-justify text-sm text-gray-700'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis dicta soluta placeat minus, delectus sint veritatis, ipsum id incidunt aperiam
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis dicta soluta placeat minus, delectus sint veritatis, ipsum id incidunt aperiam
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis dicta soluta placeat minus, delectus sint veritatis, ipsum id incidunt aperiam
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis dicta soluta placeat minus, delectus sint veritatis, ipsum id incidunt aperiam
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis dicta soluta placeat minus, delectus sint veritatis, ipsum id incidunt aperiamLorem ipsum dolor sit amet consectetur adipisicing elit. Nobis dicta soluta placeat minus, delectus sint veritatis, ipsum id incidunt aperiamLorem ipsum dolor sit amet consectetur adipisicing elit. Nobis dicta soluta placeat minus, delectus sint veritatis, ipsum id incidunt aperiam
+                        </p>
+                        <button className='text-md my-3 px-4 border-2 border-[#00B4D8] py-1 bg-[#00B4D8] text-white rounded-full'>By Dr Rajnish Sharma</button> <span className='text-md text-gray-600 font-semibold'>| Read Time 9 Min</span>
+                    </div>
+                    <section className='my-5 md:p-7 md:border-2 border-gray-50 rounded-xl shadow-md'>
+                        <h1 className='lg:text-2xl text-xl group text-gray-900 my-2 font-semibold hover:text-[#00B4D8] duration-300'>What is the benefit after get Bone and Joint Hospital Appointment ?
+                            {/* <div className='w-[50] h-1.5 my-1 group-hover:w-[100] duration-300 rounded-full bg-[#00B4D8]'></div> */}
+                        </h1>
+                        <div className='grid lg:grid-cols-[auto_40%] md:grid-cols-[auto_35%] justify-between grid-cols-1 lg:items-center gap-10 '>
+                            <div>
+                                {/* <button className='text-md my-1 px-4 border-2 border-[#00B4D8] py-1 text-[#00B4D8] bg-white rounded-full'>By Dr Rajnish Sharma</button> */}
+                                <p className='mt-2 text-justify text-sm text-gray-700'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis dicta soluta placeat minus, delectus sint veritatis, ipsum id incidunt aperiam
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis dicta soluta placeat minus, delectus sint veritatis, ipsum id incidunt aperiam
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis dicta soluta placeat minus, delectus sint veritatis, ipsum id incidunt aperiam
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis dicta soluta placeat minus, delectus sint veritatis, ipsum id incidunt aperiam
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis dicta soluta placeat minus, delectus sint veritatis, ipsum id incidunt aperiamLorem ipsum dolor sit amet consectetur adipisicing elit. Nobis dicta soluta placeat minus, delectus sint veritatis, ipsum id incidunt aperiamLorem ipsum dolor sit amet consectetur adipisicing elit. Nobis dicta soluta placeat minus, delectus sint veritatis, ipsum id incidunt aperiam
+                                </p>
+                                <h3 className='my-3 text-lg font-semibold  text-[#00B4D8]'>Benefits of an appointment we provide </h3>
+                                <ol className=''>
+                                    {[1, 2, 3].map((item, index) => {
+                                        return (
+                                            <li key={index} className='flex text-md items-center text-gray-800 gap-2'>
+                                                <div className='w-2 h-2 animate-pulse rounded-full bg-[#00B4D8] '></div>
+                                                Hello that is the benefits of appointment at bone and joint hospital
+                                            </li>
+                                        )
+                                    })}
+                                </ol>
+                            </div>
+                            <div className='relative lg:w-full lg:h-[350] w-full h-[250] overflow-hidden cursor-pointer'>
+                                <Image fill src={'/banner.jpg'} className='w-full h-full hover:scale-[1.1] duration-300  object-cover rounded-xl' alt='blog section image' />
+                            </div>
+                        </div>
+                    </section>
+
+
+                </div>
+            </section>
+        </div>
+    )
 }
